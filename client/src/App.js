a05ff92b893fc5d523771e82d8def7e1a56d15cc
johned <johned.matt@gmail.com>
2020-11-04 17:01:59 -0600

    rendering data to artistpage comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import ArtistPage from './components/ArtistPage';
 
 import UserList from './components/UsersList';
 
 
 function App() {
 
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
+    return (
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li><NavLink to="/" activeclass="active">Home</NavLink></li>
+                    <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/artists" component={ArtistPage} />
+                <Route path="/artists/:artistname" component={ArtistPage} />
+                <Route path="/users">
+                    <UserList />
+                </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-  );
+                <Route path="/">
+                    <h1>NEW WAVE ORDER</h1>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- client/src/components/ArtistPage.js
+++ client/src/components/ArtistPage.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import nba from "../fonts/nbagang.jpg"
 // map aout actual page comps layout and review mvp plus splash page
+const artist = {
+    name: "YoungBoy Never Broke Again", yt: "https://www.youtube.com/channel/UClW4jraMKz6Qj69lJf-tODA", ig: "https://www.instagram.com/neverbrokeagainllc", spotify: "https://open.spotify.com/artist/7wlFDEWiM5OoIAt8RSli8b", twt: "https://twitter.com/ggyoungboy"
+}
+
 const ArtistPage = () => {
     return (
         <div>
-
+            {artist.name}
+            <img
+                style={{
+                    width: `auto`,
+                    height: `250px`,
+                    border: '12px solid #ffffff',
+                    borderRadius: '4em',
+                    boxShadow: '0px 1px 9px 9px black, 1px 8px 4px 4px black'
+                }}
+                src={nba} />
+            <br />
+            {artist.ig}
+            <br />
+            {artist.yt}
+            <br />
+            {artist.spotify}
+            <br />
+            {artist.twt}
         </div>
     );
 };
