c926faabcbc4af3342b7771e13953dbb0c802560
johned <johned.matt@gmail.com>
2020-11-04 05:44:39 -0600

    added react player and init artist profile comp

---
+++ client/src/components/ArtistPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// map aout actual page comps layout and review mvp plus splash page
+const ArtistPage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ArtistPage;
