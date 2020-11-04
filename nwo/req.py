4690dac45c450212a22b0c9aca37d44f2fd8fb11
johned <johned.matt@gmail.com>
2020-11-04 02:33:02 -0600

    added request lib and musicbrainz pkg for data testing

---
+++ nwo/req.py
@@ -0,0 +1,11 @@
+musicbrainzngs.set_useragent()
+
+artist_id = "c5c2ea1c-4bde-4f4d-bd0b-47b200bf99d6"
+try:
+    result = musicbrainzngs.get_artist_by_id(artist_id)
+except WebServiceError as exc:
+    print("Something went wrong with the request: %s" % exc)
+else:
+    artist = result["artist"]
+    print("name:\t\t%s" % artist["name"])
+    print("sort name:\t%s" % artist["sort-name"])
