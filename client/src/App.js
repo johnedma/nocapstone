96e5bcfc4ddfb2acdeae5fb30784b769f69ef891
johned <johned.matt@gmail.com>
2020-11-05 17:49:03 -0600

    added mapping of yt imgs to div with onclick to pass url to player with useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import ArtistPage from './components/ArtistPage';
 import Navbar from './components/Navbar';
@@ -10,20 +10,22 @@ import Splash from './components/Splash';
 
 function App() {
     const chartList = [
-        { title: "positions", artist: "Ariana Grande", url: "https://www.youtube.com/watch?v=tcYodQoapMg" },
-        { title: "Forever After All", artist: "Luke Combs", url: "https://www.youtube.com/watch?v=fmbLCMGtEQc" },
-        { title: "Golden", artist: "Harry Styles", url: "https://www.youtube.com/watch?v=P3cffdsEXXw" },
-        { title: "Tyler Herro", artist: "Jack Harlow", url: "https://www.youtube.com/watch?v=np9Ub1LilKU" },
-        { title: "SO DONE", artist: "The Kid Laroi", url: "https://www.youtube.com/watch?v=yasj3j76SyM" },
-        { title: "BICHOTA", artist: "Karol G", url: "https://www.youtube.com/watch?v=QaXhVryxVBk" },
-        { title: "I CAN'T STOP ME", artist: "TWICE", url: "https://www.youtube.com/watch?v=CM4CkVFmTds" },
-        { title: "Snitch", artist: "Joyner Lucas", url: "https://www.youtube.com/watch?v=74HaJcmgUBw" },
-        { title: "Promiscuous (feat. Timbaland)", artist: "Nelly Furtado", url: "https://www.youtube.com/watch?v=0J3vgcE5i2o" },
-        { title: "Damage", artist: "H.E.R.", url: "https://www.youtube.com/watch?v=PAFAfhod9TU" },
-        { title: "Hold On", artist: "Chord Overstreet", url: "https://www.youtube.com/watch?v=8ofCZObsnOo" },
-        { title: "La TÃ³xica", artist: "Farruko", url: "https://www.youtube.com/watch?v=puugRJxgdt4" },
-        { title: "Sweater Weather", artist: "The Neighbourhood", url: "https://www.youtube.com/watch?v=GCdwKhTtNNw" },
-        { title: "the story of O.J.", artist: "YoungBoy Never Broke Again", url: "https://www.youtube.com/watch?v=B2J3kLJ8PQk" }]
+        { title: "positions", artist: "Ariana Grande", url: "https://www.youtube.com/watch?v=tcYodQoapMg", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Forever After All", artist: "Luke Combs", url: "https://www.youtube.com/watch?v=fmbLCMGtEQc", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Golden", artist: "Harry Styles", url: "https://www.youtube.com/watch?v=P3cffdsEXXw", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Tyler Herro", artist: "Jack Harlow", url: "https://www.youtube.com/watch?v=np9Ub1LilKU", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "SO DONE", artist: "The KPAFAfhod9TU Laroi", url: "https://www.youtube.com/watch?v=yasj3j76SyM", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "BICHOTA", artist: "Karol G", url: "https://www.youtube.com/watch?v=QaXhVryxVBk", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "I CAN'T STOP ME", artist: "TWICE", url: "https://www.youtube.com/watch?v=CM4CkVFmTds", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Snitch", artist: "Joyner Lucas", url: "https://www.youtube.com/watch?v=74HaJcmgUBw", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Promiscuous (feat. Timbaland)", artist: "Nelly Furtado", url: "https://www.youtube.com/watch?v=0J3vgcE5i2o", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Damage", artist: "H.E.R.", url: "https://www.youtube.com/watch?v=PAFAfhod9TU", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Hold On", artist: "Chord Overstreet", url: "https://www.youtube.com/watch?v=8ofCZObsnOo", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "La TÃ³xica", artist: "Farruko", url: "https://www.youtube.com/watch?v=puugRJxgdt4", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "Sweater Weather", artist: "The Neighbourhood", url: "https://www.youtube.com/watch?v=GCdwKhTtNNw", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" },
+        { title: "the story of O.J.", artist: "YoungBoy Never Broke Again", url: "https://www.youtube.com/watch?v=B2J3kLJ8PQk", cover: "http://img.youtube.com/vi/PAFAfhod9TU/0.jpg" }]
+    const [currentSong, setCurrentSong] = useState('https://www.youtube.com/watch?v=oUFJJNQGwhk')
+
 
 
     return (
@@ -38,17 +40,23 @@ function App() {
 
                 <Route path="/">
                     {/* <Splash /> */}
-                    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    {chartList.map((item, i) =>
+                        <div key={i}><img src={item.cover}
+                            onClick={() => setCurrentSong(item.url)}
+                        /></div>
+
+                    )}
+                    <ReactPlayer url={currentSong} />
                     -----------------
-                <ReactPlayer
+                {/* <ReactPlayer
                         url={[
                             'https://www.youtube.com/watch?v=oUFJJNQGwhk',
                             'https://www.youtube.com/watch?v=jNgP6d9HraI'
                         ]}
-                    /> React
+                    />  */}
 
 
-{/* player custo */}
+                    {/* player custo */}
                     {/* top level needs container to make display flex inline grid behavior */}
                     {/* 2nd level nested div
     width: 50%;
