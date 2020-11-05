880b5dcd3ef5afaa2f58f7eb0d5d502134f6d0f4
johned <johned.matt@gmail.com>
2020-11-05 12:00:31 -0600

    adding chart data to map over in app

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import ArtistPage from './components/ArtistPage';
 import Navbar from './components/Navbar';
 
 import UserList from './components/UsersList';
+import ReactPlayer from 'react-player'
 
 
 function App() {
@@ -19,6 +20,15 @@ function App() {
                 </Route>
 
                 <Route path="/">
+                    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    -----------------
+                <ReactPlayer
+                        url={[
+                            'https://www.youtube.com/watch?v=oUFJJNQGwhk',
+                            'https://www.youtube.com/watch?v=jNgP6d9HraI'
+                        ]}
+                    /> React
+
                     {/* <h1>NEW WAVE ORDER</h1> */}
                 </Route>
             </Switch>
---
+++ client/src/components/OTW.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OTW = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default OTW;
