2e6627b100e2eda80a4a52d4f00075e54143411c
johned <johned.matt@gmail.com>
2020-11-08 14:17:58 -0600

    added class methods to user model pw hashing and querying

--- nwo/models.py
+++ nwo/models.py
@@ -6,12 +6,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    password_digest = db.Column(db.String(255), nullable=False)
 
     def to_dict(self):
         return {
@@ -19,3 +20,16 @@ class User(db.Model):
             "username": self.username,
             "email": self.email
         }
+
+    @property
+    def password(self):
+        raise AttributeError('Password not readable.')
+
+    @password.setter
+    def password(self, password):
+        self.password_digest = generate_password_hash(password)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        user = cls.query.filter(User.username == username).scalar()
+        return check_password_hash(user.password_digest, password), user
