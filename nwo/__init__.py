be6ada653c325f45ce0cbac139217e2665d0e3b1
johned <johned.matt@gmail.com>
2020-11-08 13:36:19 -0600

    updade to init.py, importing flask_login comps

--- nwo/__init__.py
+++ nwo/__init__.py
@@ -2,28 +2,53 @@ import os
 from flask import Flask, render_template, request, session
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_user,
+    logout_user,
+    login_required
+)
 
+from nwo.models import db, User
+from nwo.api.user_routes import user_routes
+from nwo_app.api.auth_routes import auth_routes
 
-from starter_app.models import db, User
-from starter_app.api.user_routes import user_routes
-
-from starter_app.config import Config
+from nwo.config import Config
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 db.init_app(app)
+login_manager = LoginManager(app)
 
-## Application Security
+# Application Security
 CORS(app)
+CSRFProtect(app)
+
+
+@login_manager.user_loaded
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
+
+
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie('csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') else False,
-        samesite='Strict' if os.environ.get('FLASK_ENV') else None,
-        httponly=True)
+                        generate_csrf(),
+                        secure=True if os.environ.get('FLASK_ENV') else False,
+                        samesite='Strict' if os.environ.get(
+                            'FLASK_ENV') else None,
+                        httponly=True)
     return response
 
 
