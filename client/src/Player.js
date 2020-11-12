1e19ebebd633091cff8729cabf4748261ef3f85c
johned <johned.matt@gmail.com>
2020-11-12 11:25:01 -0600

    added next and back btn to player

--- client/src/Player.js
+++ client/src/Player.js
@@ -6,20 +6,37 @@ const Player = ({ currentSong }) => {
     // const [currentSong, setCurrentSong] = useState("https://www.youtube.com/watch?v=0J3vgcE5i2o")
     const [play, setPlay] = useState(false)
     const updatePlay = () => !play ? setPlay(true) : setPlay(false)
-    // useEffect(() => { }, [currentSong])
-    console.log(play);
+    const [next, setNext] = useState(null)
+    const updateNext = () =>
+
+        // useEffect(() => { }, [currentSong])
+        console.log(play);
     return (
         <>
             <div className="footer" >
+                <button style={{ background: `none`, border: `none` }}
+                    onClick={() => updateNext()}>
+                    <i className="far fa-caret-square-left"
+                        style={{
+                            fontSize: `2em`,
+                            color: `deeppink`,
+                            cursor: `pointer`,
+                            // border: `solid 2px #9e9e9e3b`,
+                            borderRadius: `1em`,
+                            padding: `3px`,
+                            boxShadow: `-2px 2px 4px 1px #80888cab, 1px 2px 5px 2px #b3ced8ad`
+                        }}
+                    />
+                </button>
                 <button style={{ background: `none`, border: `none` }}
                     onClick={() => updatePlay()}>
                     {/* {play === false ? */}
                     <i className={(play === false) ? "far fa-play-circle" : "far fa-pause-circle"}
                         style={{
-                            fontSize: `3em`,
+                            fontSize: `2em`,
                             color: `deeppink`,
                             cursor: `pointer`,
-                            border: `solid 3px #9e9e9e3b`,
+                            // border: `solid 2px #9e9e9e3b`,
                             borderRadius: `1em`,
                             padding: `3px`,
                             boxShadow: `-2px 2px 4px 1px #80888cab, 1px 2px 5px 2px #b3ced8ad`
@@ -27,7 +44,7 @@ const Player = ({ currentSong }) => {
                     />
                     {/* : <i className="far fa-pause-circle"
                             style={{
-                                fontSize: `3em`,
+                                fontSize: `2em`,
                                 color: `deeppink`,
                                 cursor: `pointer`,
                                 border: `solid 3px #9e9e9e3b`,
@@ -36,6 +53,21 @@ const Player = ({ currentSong }) => {
                                 boxShadow: `-2px 2px 4px 1px #80888cab, 1px 2px 5px 2px #b3ced8ad`
                             }}
                         />} */}
+                </button>
+                <button style={{ background: `none`, border: `none` }}
+                // onClick={() => updateNext()}
+                >
+                    <i className="far fa-caret-square-right"
+                        style={{
+                            fontSize: `2em`,
+                            color: `deeppink`,
+                            cursor: `pointer`,
+                            // border: `solid 2px #9e9e9e3b`,
+                            borderRadius: `1em`,
+                            padding: `3px`,
+                            boxShadow: `-2px 2px 4px 1px #80888cab, 1px 2px 5px 2px #b3ced8ad`
+                        }}
+                    />
                 </button>
             </div>
             <ReactPlayer style={{ display: `none` }}
