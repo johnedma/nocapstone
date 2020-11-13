663795f7bf08d57504cf4cc83c75e6815fd6c3b3
johned <johned.matt@gmail.com>
2020-11-13 16:26:15 -0600

    attr line added

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -50,6 +50,7 @@ function LoginForm(props) {
                 </div>
             </div>
             <button>Login</button>
+            Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
         </form>
     );
 }
