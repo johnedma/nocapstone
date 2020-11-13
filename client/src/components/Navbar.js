bf2ba87b18593e4a3b99459625f38cfba984aea3
johned <johned.matt@gmail.com>
2020-11-13 16:43:23 -0600

    added last nav item for mvp

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { ReactComponent as ProfileBtn } from '../assets/imgs/surf-board.svg';
 import { ReactComponent as NewsBtn } from '../assets/imgs/surf.svg';
+import { ReactComponent as LogOutBtn } from '../assets/imgs/peaceout.svg';
 import AuthContext from '../auth';
 
 const Navbar = () => {
@@ -34,6 +35,18 @@ const Navbar = () => {
                 }} />
             </NavLink>
             <ProfileBtn
+                // onClick={() => logoutUser()}
+                style={{
+                    width: `45px`, height: `auto`,
+                    width: `45px`,
+                    height: `auto`,
+                    /* box-shadow: rgb(72, 157, 207) 0px 5px 15px 10px, rgba(96, 125, 139, 0.51) 0px 3px 5px 6px; */
+                    padding: `0.5em`,
+                    border: `5px solid rgba(56, 118, 154, 0.11)`,
+                    borderRadius: `1em`
+
+                }} />
+            <LogOutBtn
                 onClick={() => logoutUser()}
                 style={{
                     width: `45px`, height: `auto`,
