062b6d6bb435f47b68dcf494989ff33f69d7162e
johned <johned.matt@gmail.com>
2020-11-15 17:33:57 -0600

    import forms to splash

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import splashbg from "../fonts/nuwave.jpg"
+import LoginForm from './LoginForm';
+import SignUp from './SignUp';
+
 
 const Splash = () => {
     return (
         <div className="splash" >
-            <img src={splashbg} />
+            <img src={splashbg}
+                style={
+                    {
+                        height: `-webkit-fill-available`,
+                        maxWidth: `-webkit-fill-available`
+                    }} />
+            <LoginForm />
+            <SignUp />
         </div>
     );
 };
